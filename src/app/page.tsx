3ed822e2eb5d203cb0c42232a023d75d49f06aa8
sagarvionsys <163301810+sagarvionsys@users.noreply.github.com>
2024-04-20 10:57:55 +0530

    Revert "Dev sagar"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,12 @@
 "use client";
 import React from "react";
 import { Boxes } from "@/components/ui/background-boxes";
-import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
-import { HeroParallax } from "@/components/ui/hero-parallax";
-import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 import { cn } from "@/utils/cn";
-import { products } from "@/Data/HeroProducts";
-import Link from "next/link";
-import Image from "next/image";
-
 const Home = () => {
-  const words = [
-    {
-      text: "Build",
-    },
-    {
-      text: "awesome",
-    },
-    {
-      text: "career",
-    },
-    {
-      text: "with",
-    },
-    {
-      text: "Fusion Institute.",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
-
   return (
     <>
       {/* hero section  */}
-      <section>
-        <HeroParallax products={products} />;
-      </section>
+      <section></section>
       {/* box glowing pattern */}
       <section className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
         <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
@@ -48,25 +20,6 @@ const Home = () => {
           Placement Record. Industry Focused Trainers. Software Testing.Java
           Development·Python-Data Science etc.
         </p>
-      </section>
-
-      {/* typewriting section */}
-      <section>
-        <div className="flex flex-col items-center justify-center h-[25rem]  ">
-          <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-            The road to freedom starts from here
-          </p>
-          <TypewriterEffectSmooth words={words} />
-
-          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-            <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
-              <Link href={"/SignIn"}>SignIn</Link>
-            </button>
-            <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-              <Link href={"/SignUp"}>SignUp</Link>
-            </button>
-          </div>
-        </div>
       </section>
     </>
   );
