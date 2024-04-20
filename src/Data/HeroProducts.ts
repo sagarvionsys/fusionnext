24a7164b7866cf8795ee814cda0dd06f95055cbf
sagarvionsys <163301810+sagarvionsys@users.noreply.github.com>
2024-04-20 10:59:08 +0530

    Revert "Revert "Dev sagar""

---
+++ src/Data/HeroProducts.ts
@@ -0,0 +1,80 @@
+export const products = [
+  {
+    title: "Moonbeam", //
+    link: "https://gomoonbeam.com",
+    thumbnail: "/img/fusion1.jpg",
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so", //
+    thumbnail: "/img/fusion2.jpg",
+  },
+  {
+    title: "Rogue", //
+    link: "https://userogue.com",
+    thumbnail: "/img/fusion3.jpg",
+  },
+
+  {
+    title: "Editorially", //
+    link: "https://editorially.org",
+    thumbnail: "/img/fusion4.jpg",
+  },
+  {
+    title: "Editrix AI",
+    link: "https://editrix.ai",
+    thumbnail: "/img/fusion5.jpg",
+  },
+  {
+    title: "Pixel Perfect", //
+    link: "https://app.pixelperfect.quest",
+    thumbnail: "/img/fusion6.jpg",
+  },
+
+  {
+    title: "Algochurn", //
+    link: "https://algochurn.com",
+    thumbnail: "/img/fusion7.jpg",
+  },
+  {
+    title: "Aceternity UI", //
+    link: "https://ui.aceternity.com",
+    thumbnail: "/img/fusion8.jpg",
+  },
+  {
+    title: "Tailwind Master Kit", //
+    link: "https://tailwindmasterkit.com",
+    thumbnail: "/img/fusion9.jpg",
+  },
+  {
+    title: "SmartBridge",
+    link: "https://smartbridgetech.com",
+    thumbnail: "/img/fusion10.jpg",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "https://renderwork.studio",
+    thumbnail: "/img/fusion11.jpg",
+  },
+
+  {
+    title: "Creme Digital", //
+    link: "https://cremedigital.com",
+    thumbnail: "/img/fusion12.jpg",
+  },
+  {
+    title: "Golden Bells Academy", //
+    link: "https://goldenbellsacademy.com",
+    thumbnail: "/img/fusion13.jpg",
+  },
+  {
+    title: "Invoker Labs", //
+    link: "https://invoker.lol",
+    thumbnail: "/img/fusion14.jpeg",
+  },
+  {
+    title: "E Free Invoice", //
+    link: "https://efreeinvoice.com",
+    thumbnail: "/img/fusion15.jpg",
+  },
+];
