0c64893c6f59fa1c1208ff6cf2c589d178b46125
Rutuja-Vionsys <Rutujakerkar@vionsys.com>
2024-04-29 16:31:26 +0530

    Non-IT career page

---
+++ src/Data/non-it.ts
@@ -0,0 +1,39 @@
+const nonIt=[
+    {id:1, role:"Admission counselor",experience:"2+ yrs",job:"part/full time",
+        jobDescription:"Offering information on programs, assisting with application materials, providing guidance on admissions requirements facilitating communication between applicants and academic departments.serve as a liaison between the institution and applicants, offering support and answering questions to ensure a smooth admissions process."
+    },
+    {
+        id:2, role:"Business Development Associate", experience:"2+ yrs", job:"part/full time",
+        jobDescription:"Generate leads, contacting potential clients, promoting training offerings,develop relationships with corporate clients, negotiate partnerships,organize promotional events,gather feedback, and collaborate with internal teams."
+    },
+    {
+        id:3, role:"Business Development Executive", experience:"2+ yrs", job:"part/full time",
+        jobDescription:"Generate leads, nurture client relationships, expand the company's market reach,identifying new business opportunities, pitching products or services to potential clients, conduct market research,collaborate with internal teams to tailor offerings to client needs and contribute to overall business growth."
+    },
+    {
+        id:4, role:"Front desk Executive", experience:"2+ yrs", job:"part/full time",
+        jobDescription:"Should possess strong customer service and communication skills, along with organizational abilities and attention to detail. They must present a professional demeanor, demonstrate problem-solving capabilities, and ideally have basic technical proficiency."
+    },
+    {
+        id:5, role:"Team lead", experience:"2+ yrs", job:"full time",
+        jobDescription:"As a Team Lead you will be responsible for oversee team members, participate in recruitment, provide training, manage performance, plan projects, allocate resources, solve problems, facilitate communication, ensure quality, and promote continuous improvement."
+    },
+    {
+        id:6, role:"Manager", experience:"2+ yrs", job:"full time",
+        jobDescription:"oversee teams, set goals, and ensure tasks are completed efficiently,provide guidance, resolve conflicts,evaluate performance, communicate with stakeholders, allocate resources, and drive organizational objectives while adhering to policies and regulations."
+    },
+    {
+        id:7, role:"HR Executive", experience:"2+ yrs", job:"full time",
+        jobDescription:"Recruitment and onboarding of new employees, administering employee benefits and payroll, handle employee relations issues, performance management processes, and training and development initiatives, collaborate with other departments to support organizational objectives and foster a positive work environment."
+    },
+    {
+        id:8, role:"HR and Placement Executive", experience:"2+ yrs", job:"full time",
+        jobDescription:"Manage human resources functions and placement activities, conduct interviews, coordinate placement activities for students. manage employee relations, administer benefits, cultivate relationships with industry partners to facilitate internships, job placements, and career development opportunities for students."
+    },
+    {
+        id:9, role:"Training and Placement Officer", experience:"2+ yrs", job:"full time",
+        jobDescription:"Coordinate training programs, workshops, and seminars to enhance skills and employability, establish relationships with industry partners to organize internships, job placements, and recruitment drives. provide career counseling, resume assistance, and interview preparation support to individuals."
+    }
+]
+
+export default nonIt;
---
+++ src/components/Apply.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import { useState } from 'react';
+import { Modal, message } from 'antd';
+
+
+
+function Apply() {
+
+    const[name,setName]=useState('');
+    const[email,setEmail]=useState('');
+    const[phone,setPhone]=useState('');
+    const[job,setJob]=useState('');
+    const[experience,setExperience]=useState('');
+    const[resume,setResume]=useState('');
+
+    const logInputValue = () => {
+        const data={name,email,phone,job,experience,resume}
+        console.log(data);
+        message.success("Applied Successfully!");
+        handleCancel();
+        setName("");
+        setEmail("");
+        setPhone("");
+        setJob("");
+        setExperience("");
+        setResume("");
+    };
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+  return (
+    <div className='flex items-center justify-center'>
+        <button onClick={showModal} className="block w-20 max-w-xs mx-au to bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-1 py-1 font-semibold">Apply</button>
+        <Modal footer={null} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <h2 className='flex items-center justify-center font-bold text-xl pb-10 pt-2'>Application form</h2>
+        <div className="flex -mx-3">
+          <div className="w-full px-3 mb-5">
+            <label className="text-xs md:text-sm font-semibold px-1">Full Name<span className='text-red-700'> *</span></label>
+            <div className="flex">
+              <input type="text" id="Name" className="w-full bg-gray-100 -ml-15 pl-5 sm:pl-10 pr-1 py-1 sm:pr-3 sm:py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)} ></input>
+            </div>
+          </div>
+        </div>
+        <div className="flex -mx-3">
+          <div className="w-full px-3 mb-5">
+            <label className="text-xs md:text-sm font-semibold px-1">Email Address<span className='text-red-700'> *</span></label>
+            <div className="flex">
+              <input type="email" className="w-full bg-gray-100 -ml-15 pl-5 sm:pl-10 pr-1 py-1 sm:pr-3 sm:py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Enter email address" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+            </div>
+          </div>
+        </div>
+        <div className="flex -mx-3">
+          <div className="w-full px-3 mb-5">
+            <label className="text-xs md:text-sm font-semibold px-1">Mobile number<span className='text-red-700'> *</span></label>
+            <div className="flex">
+              <input type="tel" className="w-full bg-gray-100 -ml-15 pl-5 sm:pl-10 pr-1 py-1 sm:pr-3 sm:py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Enter mobile number" value={phone} onChange={(e) => setPhone(e.target.value)}></input>
+            </div>
+          </div>
+        </div>
+        <div className="flex -mx-3">
+          <div className="w-full px-3 mb-5">
+            <label className="text-xs md:text-sm font-semibold px-1">Job Position Applied for<span className='text-red-700'> *</span></label>
+            <div className="flex">
+              <input type="text" id="Name" className="w-full bg-gray-100 -ml-15 pl-5 sm:pl-10 pr-1 py-1 sm:pr-3 sm:py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Enter position applied for" value={job} onChange={(e) => setJob(e.target.value)}></input>
+            </div>
+          </div>
+        </div> 
+        <div className="flex -mx-3">
+          <div className="w-full px-3 mb-5">
+            <label className="text-xs md:text-sm font-semibold px-1">Years of Experience<span className='text-red-700'> *</span></label>
+            <div className="flex">
+              <select className="w-full bg-gray-100 -ml-15 pl-5 sm:pl-10 pr-1 py-1 sm:pr-3 sm:py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"  value={experience} onChange={(e) => setExperience(e.target.value)} >
+                  <option>Please Select</option>
+                  <option>less than 1 year</option>
+                  <option>1-3 years</option>
+                  <option>3-5 years</option>
+                  <option>5+ years</option>
+              </select>
+            </div>
+          </div>
+        </div>  
+        <div className="flex -mx-3">
+          <div className="w-full px-3 mb-5">
+            <label className="text-xs md:text-sm font-semibold px-1">Resume<span className='text-red-700'> *</span></label>
+            <div className="flex">
+              <input type="file" className="w-full bg-gray-100 -ml-15 pl-2 md:pl-6 pr-1 py-1 sm:pr-3 sm:py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Upload resume" value={resume} onChange={(e) => setResume(e.target.value)} ></input>
+            </div>
+          </div>
+        </div>  
+        <div className="flex -mx-3">
+          <div className="w-full px-3 mb-5">
+            <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-1 py-1 sm:px-3 sm:py-3 font-semibold" onClick={logInputValue}>Apply</button>
+          </div>
+        </div>                   
+        </Modal>
+    </div>
+  )
+}
+
+export default Apply
