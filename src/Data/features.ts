a4ef8df77c64416df53879cfc01da214051c4f33
Rutuja-Vionsys <Rutujakerkar@vionsys.com>
2024-05-06 15:07:25 +0530

    UI changes made in about us and non-it profile

--- src/Data/features.ts
+++ src/Data/features.ts
@@ -7,11 +7,11 @@ import { FaPeopleCarryBox } from "react-icons/fa6";
 import { FaChalkboardTeacher } from "react-icons/fa";
 
 const features=[
-    {id:1,title:"Limited seat batches",Icon:IoPeopleOutline, color:"FFBB70"},
-    {id:2,title:"Trainer-led sessions",Icon:FaChalkboardTeacher, color:"A8CD9F"},
+    {id:1,title:"Limited Seat Batches",Icon:IoPeopleOutline, color:"FFBB70"},
+    {id:2,title:"Trainer-Led Sessions",Icon:FaChalkboardTeacher, color:"A8CD9F"},
     {id:3,title:"Effective Resume Building",Icon:IoNewspaperOutline, color:"FFCDEA"},
     {id:4,title:"Weekly Mock Interviews",Icon:PiHandshakeFill, color:"D0BFFF"},
     {id:5,title:"Building Leadership Skills",Icon:FaPeopleGroup, color:"E9C874"},
-    {id:6,title:"Creating Team work spirit",Icon:FaPeopleCarryBox, color:"80B3FF"},
+    {id:6,title:"Creating Team Work Spirit",Icon:FaPeopleCarryBox, color:"80B3FF"},
 ]
 export default features;
